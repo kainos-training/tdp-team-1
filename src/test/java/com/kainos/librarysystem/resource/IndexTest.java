d388b4f769098d0f37c7f5772f036085e2f3aea7
Hannah <H.Smith@kainos.com>
2015-09-10 10:09:58 +0100

    Add Unit test for mocked db connection and returning View of type index. Changes to pom.xml. Now connection to db instead of hard coded data.

--- src/test/java/com/kainos/librarysystem/resource/IndexTest.java
+++ src/test/java/com/kainos/librarysystem/resource/IndexTest.java
@@ -1,11 +1,37 @@
 package com.kainos.librarysystem.resource;
 
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+
+import io.dropwizard.views.View;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.mockito.Mockito.*;
+
 import org.junit.Test;
+
+import com.kainos.libarysystem.dbConnector.DbConnector;
+import com.kainos.librarysystem.models.Book;
+import com.kainos.librarysystem.views.Index;
 
 public class IndexTest {
 	
+	
 	@Test
-	public void exampleTest() {
-		assert(true);
-	}
+	public void returnsIndexViewTest() throws Exception{
+		// Set up mocking
+		DbConnector connector = mock(DbConnector.class);
+		when(connector.getBooksFromDB()).thenReturn(new ArrayList<Book>());
+		
+		// Call test method
+		ViewsResource viewsResource = new ViewsResource(connector);
+		
+		try {
+			View response = viewsResource.index();
+			assertThat(response, instanceOf(Index.class));
+			verify(connector.getBooksFromDB());
+		} catch(Exception e) {
+			assert(false);
+		}
+	} 
 }
