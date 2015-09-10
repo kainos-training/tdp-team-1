81a29317e714eb6fc16980d8f4505b39e29eae1d
Adam D <a.dickey@kainos.com>
2015-09-10 14:55:52 +0100

    Add updated ftl and BookDetail class

--- src/main/java/com/kainos/librarysystem/views/BookDetails.java
+++ src/main/java/com/kainos/librarysystem/views/BookDetails.java
@@ -1,9 +1,26 @@
 package com.kainos.librarysystem.views;
 
-public class BookDetails {
+import com.kainos.librarysystem.models.Book;
 
-	public BookDetails() {
-		// TODO Auto-generated constructor stub
+import io.dropwizard.views.View;
+
+public class BookDetails extends View {
+
+	private Book book;
+	
+	public BookDetails(Book book) {
+		super("/bookDetails.ftl");
+		this.book = book;
 	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	
 
 }
