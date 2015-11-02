33352bb6a67ac1c62f4e7f68110fa194832ddba9
MGough <mbg317@bham.ac.uk>
2015-11-02 14:06:47 +0000

    List all frameworks method complete but untested

--- src/main/java/database/JDBCConnector.java
+++ src/main/java/database/JDBCConnector.java
@@ -2,7 +2,13 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kainos.projectdrill.model.framework.Framework;
 
 public class JDBCConnector {
 
@@ -16,7 +22,23 @@ public class JDBCConnector {
 	public JDBCConnector() throws ClassNotFoundException, SQLException {
 		Class.forName(DRIVER);
 		
-		connection = DriverManager.getConnection(DB_ADDRESS);
+		connection = DriverManager.getConnection(DB_ADDRESS);	
+	}
+	
+	public List<Framework> selectAllFrameworks() throws SQLException {
+		PreparedStatement statement = connection.prepareStatement("SELECT id, frameworkName, license, expert, vendor FROM framework");
+		
+		ResultSet results = statement.executeQuery();
+		
+		List<Framework> frameworkList = new ArrayList<Framework>();
+		
+		while (results.next()) {
+			Framework newFramework = new Framework(results.getInt(1), results.getString(2), results.getString(3), results.getString(4), results.getString(5));
+			frameworkList.add(newFramework);
+		}
+		
+		return frameworkList;
+		
 		
 	}
 }
