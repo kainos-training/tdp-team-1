c035f16da5e25c8859344194064bf03ea963b1f7
Eduardo Lima <e.lima@kainos.com>
2015-11-03 11:24:37 +0000

    added test for project list

--- src/test/java/com/kainos/projectdrill/resource/ViewsResourceTest.java
+++ src/test/java/com/kainos/projectdrill/resource/ViewsResourceTest.java
@@ -7,6 +7,7 @@ import io.dropwizard.views.View;
 import com.kainos.projectdrill.database.JDBCConnector;
 import com.kainos.projectdrill.model.framework.Framework;
 import com.kainos.projectdrill.views.Index;
+import com.kainos.projectdrill.views.selectOneFramework;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
@@ -21,19 +22,29 @@ public class ViewsResourceTest {
 	
 	JDBCConnector mockClient = mock(JDBCConnector.class);
 	List<Framework> frameworks =  new ArrayList<Framework>();
+	List<String> project =  new ArrayList<String>();
 	ViewsResource resource;
 	
 	@Test
 	public void testIfReturnsFrameworkList() throws SQLException, ClassNotFoundException {
-		
-		when(mockClient.selectAllFrameworks()).thenReturn(frameworks);
-		
+				
 		resource = new ViewsResource(mockClient);
 		View actualView = resource.getFrameworkList();
 		
 		assertTrue(actualView instanceof Index);
 		
 		verify(mockClient).selectAllFrameworks();
+	}
+	
+	@Test
+	public void testIfReturnsProjectList() throws SQLException, ClassNotFoundException {
+				
+		resource = new ViewsResource(mockClient);
+		View actualView = resource.selectOneFramework(1);
+		
+		assertTrue(actualView instanceof selectOneFramework);
+		
+		verify(mockClient).selectProjectsForFramework(1);
 	}
 	
 	@Test
