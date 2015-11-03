b0bc83e44364ca13973434e1590f138e96bc375e
Cormac C <ccurran985@icloud.com>
2015-11-03 09:29:52 +0000

    SQL running for story 5. Seems to to work alright

--- db/dbInit.sql
+++ db/dbInit.sql
@@ -12,10 +12,31 @@ CREATE TABLE framework (
 	vendor varchar(25)
 );
 
+CREATE TABLE project (
+	id int primary key auto_increment,
+	projectName varchar(50) NOT NULL
+);
+
+CREATE TABLE projectFramework (
+	projectId int,
+	frameworkId int,
+	FOREIGN KEY (projectId) REFERENCES project(id),
+	FOREIGN KEY (frameworkId) REFERENCES framework(id)
+);
+
 DELIMITER //
 CREATE PROCEDURE insertFramework(frameworkName varchar(50), license varchar(50), expert varchar(25), vendor varchar(25))
 BEGIN
 	INSERT INTO framework(frameworkName, license, expert, vendor) VALUES(frameworkName, license, expert, vendor);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE getProjects(frameworkId int)
+BEGIN
+	SELECT projectName
+	FROM framework as fw, projectFramework as pf, project as pr
+	WHERE frameworkId = fw.id AND fw.id = pf.frameworkId;
 END //
 DELIMITER ;
 
@@ -33,8 +54,25 @@ INSERT INTO framework(frameworkName, license, expert, vendor) VALUES('Unified.js
 INSERT INTO framework(frameworkName, license, expert, vendor) VALUES('Zend Framework', 'New BSD', 'Steven', 'Alphabet');
 INSERT INTO framework(frameworkName, license, expert, vendor) VALUES('Sintra', 'MIT', 'James', 'Alphabet');
 INSERT INTO framework(frameworkName, license, expert, vendor) VALUES('Ruby On Rails', 'MIT', 'Andy', 'Alphabet');
-INSERT INTO framework(frameworkName, license, expert, vendor) VALUES('Drupql', 'GPLv2', 'Tanya', 'Alphabet');
+INSERT INTO framework(frameworkName, license, expert, vendor) VALUES('Drupal', 'GPLv2', 'Tanya', 'Alphabet');
 
+INSERT INTO project(projectName) VALUES('Digital Dictation');
+INSERT INTO project(projectName) VALUES('Evolve Core');
+INSERT INTO project(projectName) VALUES('Evolve for iPad');
+INSERT INTO project(projectName) VALUES('Medical Image Viewer');
+INSERT INTO project(projectName) VALUES('DVSA');
+INSERT INTO project(projectName) VALUES('DFRA');
+INSERT INTO project(projectName) VALUES('FCO');
+
+INSERT INTO projectFramework(projectId, frameworkId) VALUES(3, 5);
+INSERT INTO projectFramework(projectId, frameworkId) VALUES(4, 7);
+INSERT INTO projectFramework(projectId, frameworkId) VALUES(3, 8);
+INSERT INTO projectFramework(projectId, frameworkId) VALUES(2, 6);
+INSERT INTO projectFramework(projectId, frameworkId) VALUES(7, 9);
+INSERT INTO projectFramework(projectId, frameworkId) VALUES(3, 3);
+INSERT INTO projectFramework(projectId, frameworkId) VALUES(3, 1);
+INSERT INTO projectFramework(projectId, frameworkId) VALUES(4, 4);
+INSERT INTO projectFramework(projectId, frameworkId) VALUES(4, 2);
 
 DROP USER 'ktech_user'@'localhost';
 CREATE USER 'ktech_user'@'localhost' IDENTIFIED BY '1Password2';
