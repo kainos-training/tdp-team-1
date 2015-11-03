2415b1c56247789976a78aa683e8f1c873ab8470
Simon Watson <S.Watson@kainos.com>
2015-11-03 08:27:49 +0000

    Cleaning up project, removing unused code.

--- src/main/java/com/kainos/projectdrill/views/Index.java
+++ src/main/java/com/kainos/projectdrill/views/Index.java
@@ -1,7 +1,6 @@
 package com.kainos.projectdrill.views;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.kainos.projectdrill.model.framework.Framework;
@@ -13,18 +12,7 @@ public class Index extends View {
 	
 	public Index(List<Framework> frameworks) throws SQLException {
 		super("/index.ftl");
-		
 		this.frameworks = frameworks;
-		
-		// database.selectAllFrameworks();
-		
-		/* this.frameworks = new ArrayList<Framework>();
-		this.frameworks.add(new Framework(1, "Java", "0.2", "Bob Smith", "Oracle"));
-		this.frameworks.add(new Framework(1, "JavaScript", "0.2", "Bob Smith", "Oracle"));
-		this.frameworks.add(new Framework(1, "PHP", "0.2", "Bob Smith", "Oracle"));
-		this.frameworks.add(new Framework(1, "Angular", "0.2", "Bob Smith", "Oracle"));
-		this.frameworks.add(new Framework(1, "GO", "0.2", "Bob Smith", "Oracle")); */
-	
 	}
 	
 	public List<Framework> getFrameworks() {
